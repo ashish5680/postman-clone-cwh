640c9bf43dde8b9554961a1f81c32bfe37cc9ff8
ash123g <ashishgarg5680@gmail.com>
2022-03-12 13:22:29 +0530

    update

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-console.log('This is my PostMaster Project (Clone of JavaScript');
+console.log('This is my PostMaster Project (PostMan Clone)');
+
+console.log("\n");
+
 
 
 
@@ -10,19 +13,29 @@ function getElementFromString(string) {
     return div.firstElementChild;
 }
 
-// Initialize no of parameters
-let addedParamCount = 0;
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
+
+
 
 // Hide the parameters box initially
 let parametersBox = document.getElementById('parametersBox');
 parametersBox.style.display = 'none';
+
+
 
 // If the user clicks on params box, hide the json box
 let paramsRadio = document.getElementById('paramsRadio');
 paramsRadio.addEventListener('click', () => {
     document.getElementById('requestJsonBox').style.display = 'none';
     document.getElementById('parametersBox').style.display = 'block';
-})
+});
+
+
 
 // If the user clicks on json box, hide the params box
 let jsonRadio = document.getElementById('jsonRadio');
@@ -31,10 +44,30 @@ jsonRadio.addEventListener('click', () => {
     document.getElementById('parametersBox').style.display = 'none';
 })
 
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////
+
+
+
+// Initialize no of parameters
+let addedParamCount = 0;
+
+
+
+
+
 // If the user clicks on + button, add more parameters
 let addParam = document.getElementById('addParam');
+
 addParam.addEventListener('click', () => {
+
     let params = document.getElementById('params');
+
     let string = `<div class="form-row my-2">
                     <label for="url" class="col-sm-2 col-form-label">Parameter ${addedParamCount + 2}</label>
                     <div class="col-md-4">
@@ -44,23 +77,42 @@ addParam.addEventListener('click', () => {
                         <input type="text" class="form-control" id="parameterValue${addedParamCount + 2}" placeholder="Enter Parameter ${addedParamCount + 2} Value">
                     </div>
                     <button class="btn btn-primary deleteParam"> - </button>
-                    </div>`;
+                  </div>`;
+
     // Convert the element string to DOM node
     let paramElement = getElementFromString(string);
+    // console.log(paramElement);
     params.appendChild(paramElement);
+
     // Add an event listener to remove the parameter on clicking - button
     let deleteParam = document.getElementsByClassName('deleteParam');
     for (item of deleteParam) {
         item.addEventListener('click', (e) => {
-            // TODO: add a confirmation box to confirm parameter deletion
-            e.target.parentElement.remove();
+            // To-do: add a confirmation box to confirm parameter deletion (Optional)
+            e.target.parentElement.remove();                  // e.target means joo button click kiya gaya hai and phir .parentElement (uska parent element)
         })
     }
     addedParamCount++;
-})
+
+});
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
 // If the user clicks on submit button
 let submit = document.getElementById('submit');
+
 submit.addEventListener('click', () => {
     // Show please wait in the response box to request patience from the user
     // document.getElementById('responseJsonText').value = "Please wait.. Fetching response...";
@@ -73,25 +125,35 @@ submit.addEventListener('click', () => {
  
     // If user has used params option instead of json, collect all the parameters in an object
     if (contentType == 'params') {
-        data = {};
-        for (let i = 0; i < addedParamCount + 1; i++) {
-            if (document.getElementById('parameterKey' + (i + 1)) != undefined) {
+        data = {};  // make a blank object
+        for (let i = 0; i < addedParamCount + 1; i++) {                        // // ed default wala aur baaki joo add kiye hai parameters
+            if (document.getElementById('parameterKey' + (i + 1)) != undefined) {         // // joo param delete kar diya hai uski value too hogi nahi isiliye error naa aa je too isiliye ye condition lagai hai
                 let key = document.getElementById('parameterKey' + (i + 1)).value;
                 let value = document.getElementById('parameterValue' + (i + 1)).value;
                 data[key] = value; 
             }
         }
-        data = JSON.stringify(data);
+        data = JSON.stringify(data);      // convert object into string
     }
     else {
         data = document.getElementById('requestJsonText').value;
     }
 
+
+
+
+
     // Log all the values in the console for debugging
-    console.log('URL is ', url);
-    console.log('requestType is ', requestType);
-    console.log('contentType is ', contentType);
-    console.log('data is ', data);
+    console.log('URL is : ', url);
+    console.log('requestType is : ', requestType);
+    console.log('contentType is : ', contentType);
+    console.log('data is : ', data);
+
+
+
+
+
+
 
     // if the request type is get, invoke fetch api to create a post request
     if (requestType=='GET'){
@@ -106,11 +168,12 @@ submit.addEventListener('click', () => {
         });
     }
 
+    //if the request is POST
     else{
         fetch(url, {
             method: 'POST', 
-            body: data,
-            headers: {
+            body: data,        // // data is a string , isse ek string chaiye hoti hai
+            headers: {            //  // headers and body only post request mai chaiye hote hai
                 "Content-type": "application/json; charset=UTF-8"
               }  
         })
@@ -118,7 +181,7 @@ submit.addEventListener('click', () => {
         .then((text) =>{
             // document.getElementById('responseJsonText').value = text;
             document.getElementById('responsePrism').innerHTML = text;
-            Prism.highlightAll();
+            Prism.highlightAll();      // it highlight all the json text
         });
 
     }
